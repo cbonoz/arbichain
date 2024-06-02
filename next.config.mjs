b57bda6c9736f8737ed8a3855b2dfd6cf8525ca3
Chris Buonocore <chrisdistrict@gmail.com>
2024-06-01 23:30:40 -0400

    enable minify

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // output: 'export',
-  swcMinify: false,
   images: {
     domains: ["images.unsplash.com", "tailwindui.com", "res.cloudinary.com", "imgbb.com"],
   },
